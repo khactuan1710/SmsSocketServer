7545c7ac8f694d6084234510281c6f6a996ac6c0
NguyenKhacTuan <khachuongvn>
2024-11-22 19:24:25 +0700

    refactorr code

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
-// Khởi tạo Socket.IO
 const io = new Server(server, {
     cors: {
         origin: "*", // Cho phép mọi kết nối (dùng localhost hoặc IP)
@@ -17,24 +16,20 @@ const io = new Server(server, {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Tạo route chính để hiển thị button
 app.get('/', (req, res) => {
     res.render('index');  // render file 'index.ejs'
 });
 
-// Tạo route để giả lập việc nhấn button và gửi số điện thoại
 app.get('/click-button', (req, res) => {
-    const phoneNumber = req.query.phone;  // Lấy số điện thoại từ query string
+    const phoneNumber = req.query.phone;  
     console.log('Button clicked! Sending socket event to clients.');
-    console.log('Phone Number:', phoneNumber);  // In ra số điện thoại
+    console.log('Phone Number:', phoneNumber); 
 
-    // Gửi socket event cho tất cả các client đang kết nối
     io.emit('button_clicked', `Button was clicked! Phone number: ${phoneNumber}`);
     
     res.send(`Button click event sent to clients. Phone number: ${phoneNumber}`);
 });
 
-// Lắng nghe kết nối từ client
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
